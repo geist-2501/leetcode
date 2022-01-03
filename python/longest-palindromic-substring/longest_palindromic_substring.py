af46de046d8a219b117bfe79107733719a997f51
geist-2501 <blaironbute@gmail.com>
2022-01-03 22:29:05 +0000

    Improved (?) longest_palindromic_substring.py

--- python/longest-palindromic-substring/longest_palindromic_substring.py
+++ python/longest-palindromic-substring/longest_palindromic_substring.py
@@ -1,10 +1,11 @@
 class Solution:
     def longest_palindrome(self, s: str) -> str:
-        # Check odd palindromes.
         longest = s[0]  # s or sss etc
-        for i in range(len(s)):
-            left = i
-            right = i
+        il = 0
+        ir = 0
+        while il < len(s) and ir < len(s):
+            left = il
+            right = ir
             while left >= 0 and right < len(s) and s[left] == s[right]:
                 pal = s[left:(right + 1)]
                 if len(pal) > len(longest):
@@ -12,15 +13,10 @@ class Solution:
                 left -= 1
                 right += 1
 
-        for i in range(len(s) - 1):
-            left = i
-            right = i + 1
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                pal = s[left:(right + 1)]
-                if len(pal) > len(longest):
-                    longest = pal
-                left -= 1
-                right += 1
+            if il == ir:
+                ir += 1
+            else:
+                il += 1
 
         return longest
 
@@ -28,3 +24,5 @@ class Solution:
 if __name__ == '__main__':
     s = Solution()
     print(s.longest_palindrome("babad"))
+    print(s.longest_palindrome("cbbd"))
+    print(s.longest_palindrome("cbb"))
